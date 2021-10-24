9de21bc2605afcd115f6093aef1b4d285d638430
NightMare4Tonight <80159858+NightMare4Tonight@users.noreply.github.com>
2021-10-24 12:07:05 +0700

    animation commit
    changing animation

--- src/components/pokemanCard.svelte
+++ src/components/pokemanCard.svelte
@@ -1,11 +1,11 @@
 <script>
-  import { fade, scale } from 'svelte/transition'
+  import { fade } from 'svelte/transition'
   export let pokeman;
 </script>
 
 <div> 
   <a class="p-5 bg-gray-200 text-gray-800 text-center rounded-md shadow-sm hover:shadow-md flex flex-col items-center" 
-  href={`/pokemon/${pokeman.id}`} out:fade in:scale="{{duration: 600}}">
+  href={`/pokemon/${pokeman.id}`} in:fade="{{duration: 500}}">
   <img class="h-40 w-40" src={pokeman.image} alt={pokeman.name}>
   <h2 class="uppercase text-xl font-medium">{pokeman.id}. {pokeman.name}</h2>
   </a>
